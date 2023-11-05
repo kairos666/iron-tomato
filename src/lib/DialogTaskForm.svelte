4cc65137dcec87fafe0da79c7ae7ab50033856f7
David Maggi <musashi.miyamoto81@gmail.com>
2023-11-05 12:09:05 +0100

    refactor Dialog (prefill and way data binding clash averted)

--- src/lib/DialogTaskForm.svelte
+++ src/lib/DialogTaskForm.svelte
@@ -5,13 +5,13 @@
 
     export let open:boolean;
     export let initialTask:Task|undefined = undefined;
+    $: isEdit = !!initialTask;
 
     // handle pre filled form if edit instead of creation (if statement ensure re-evaluation between openings)
-    let formTask:{ label:string, description:string };
-    $: if(open) formTask = { 
-        label: initialTask?.label ?? "",
-        description: initialTask?.description ?? ""
-    }
+    let formTaskLabel:string = "";
+    let formTaskDescription:string = "";
+    $: if(open) formTaskLabel = initialTask?.label ?? "";
+    $: if(open) formTaskDescription = initialTask?.description ?? "";
 
     const dispatch = createEventDispatcher();
     function dispatchClose() {
@@ -20,13 +20,17 @@
 
     function onSubmit(evt:SubmitEvent) {
         evt.preventDefault();
+        const submittedFormData = new FormData((evt.target as HTMLFormElement));
+        const resultLabel = submittedFormData.get('task-label');
+        const resultDescription = submittedFormData.get('task-description');
+        if(resultLabel === null || typeof resultLabel !== 'string' || resultDescription === null || typeof resultDescription !== 'string') throw new Error('DialogTaskForm - problem with retrieving retrieve form values');
 
         // format new/edited task
         const timestamp = new Date().getTime();
         const resultTask:Task = {
             id: initialTask?.id ?? String(Math.round(timestamp / (Math.random() * 10000000))),
-            label: formTask.label,
-            description: (formTask.description !== "") ? formTask.description : undefined, 
+            label: resultLabel,
+            description: (resultDescription !== "") ? resultDescription : undefined, 
             isActive: initialTask?.isActive ?? false,
             isUrgent: initialTask?.isActive ?? false,
             isImportant: initialTask?.isActive ?? false,
@@ -36,7 +40,11 @@
         }
         
         // commit task
-        tasksStore.createTask(resultTask);
+        if(!initialTask) {
+            tasksStore.createTask(resultTask);
+        } else {
+            tasksStore.editTask(resultTask);
+        }
         dispatchClose();
     }
 </script>
@@ -44,18 +52,25 @@
 <Dialog open={ open } on:close={() => dispatchClose() }>
     <DialogOverlay class="dlg-Overlay" />
     <article class="dlg-Container">
-        <hgroup>
-            <DialogTitle>Créer une tâche</DialogTitle>
-            <DialogDescription>Définir et ajouter une nouvelle tâche à la liste.</DialogDescription>
-        </hgroup>
+        {#if isEdit}
+            <hgroup>
+                <DialogTitle>Modifier une tâche</DialogTitle>
+                <DialogDescription>Mise à jour de la tâche ciblée.</DialogDescription>
+            </hgroup>
+        {:else}
+            <hgroup>
+                <DialogTitle>Créer une tâche</DialogTitle>
+                <DialogDescription>Définir et ajouter une nouvelle tâche à la liste.</DialogDescription>
+            </hgroup>
+        {/if}
         <form on:submit={ onSubmit }>
             <label for="task-label">Libellé</label>
-            <input type="text" id="task-label" name="task-label" placeholder="Intitulé de la tâche" bind:value={ formTask.label } required />
+            <input type="text" id="task-label" name="task-label" placeholder="Intitulé de la tâche" value={ formTaskLabel } required />
             <label for="task-description">Description</label>
-            <textarea id="task-description" name="task-description" placeholder="Descriptif détaillé optionnel de la tâche" bind:value={ (formTask.description) } />
+            <textarea id="task-description" name="task-description" placeholder="Descriptif détaillé optionnel de la tâche" value={ formTaskDescription } />
             <menu class="dlg-Container_ActionsMenu">
                 <button type="reset" class="secondary outline" on:click={() => dispatchClose() }>Annuler</button>
-                <button type="submit">Créer</button>
+                <button type="submit">{#if isEdit}Modifier la tâche{:else}Créer la tâche{/if}</button>
             </menu>
         </form>
     </article>
