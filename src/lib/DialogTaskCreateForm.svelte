6e06827decdf3adae88b070e6daedba400ccac1c
David Maggi <musashi.miyamoto81@gmail.com>
2023-11-19 09:42:23 +0100

    urgency/importance in create form

--- src/lib/DialogTaskCreateForm.svelte
+++ src/lib/DialogTaskCreateForm.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { type BaseTask, taskCreate } from "../stores/persistentTasks";
-    import { Dialog, DialogOverlay, DialogTitle, DialogDescription } from "@rgossiaux/svelte-headlessui";
-    import { PlusCircle } from "lucide-svelte";
+    import { Dialog, DialogOverlay, DialogTitle, DialogDescription, Switch, SwitchGroup, SwitchLabel } from "@rgossiaux/svelte-headlessui";
+    import { AlarmClock, PlusCircle, Siren } from "lucide-svelte";
     import { appUIState } from "../stores/appUIState";
     import { tick } from "svelte";
 
@@ -10,6 +10,8 @@
     // handle pre filled form if edit instead of creation (if statement ensure re-evaluation between openings)
     let formTaskLabel:string = "";
     let formTaskDescription:string = "";
+    let formTaskIsUrgent:boolean = false;
+    let formTaskIsImportant:boolean = false;
 
     function onSubmit(evt:SubmitEvent) {
         evt.preventDefault();
@@ -23,8 +25,8 @@
             label: resultLabel,
             description: (resultDescription !== "") ? resultDescription : undefined, 
             isActive: false,
-            isUrgent: false,
-            isImportant: false,
+            isUrgent: formTaskIsUrgent,
+            isImportant: formTaskIsImportant,
             isDone: false
         }
 
@@ -52,6 +54,20 @@
             <input type="text" id="task-label" name="task-label" placeholder="Intitulé de la tâche" value={ formTaskLabel } required />
             <label for="task-description">Description</label>
             <textarea id="task-description" name="task-description" placeholder="Descriptif détaillé optionnel de la tâche" value={ formTaskDescription } />
+            <SwitchGroup class="switch-group">
+                <SwitchLabel class="switch-label"><AlarmClock /> Marquer urgent</SwitchLabel>
+                <Switch bind:checked={formTaskIsUrgent} class={formTaskIsUrgent ? "switch switch-enabled" : "switch switch-disabled"}>
+                    <span class="sr-only">Marquer urgent</span>
+                    <span class="toggle" class:toggle-on={ formTaskIsUrgent } class:toggle-off={ !formTaskIsUrgent } />
+                </Switch>
+            </SwitchGroup>
+            <SwitchGroup class="switch-group">
+                <SwitchLabel class="switch-label"><Siren /> Marquer important</SwitchLabel>
+                <Switch bind:checked={formTaskIsImportant} class={formTaskIsImportant ? "switch switch-enabled" : "switch switch-disabled"}>
+                    <span class="sr-only">Marquer important</span>
+                    <span class="toggle" class:toggle-on={ formTaskIsImportant } class:toggle-off={ !formTaskIsImportant } />
+                </Switch>
+            </SwitchGroup>
             <menu class="dlg-Container_ActionsMenu">
                 <button type="reset" class="secondary outline" on:click={() => clearModal() }>Annuler</button>
                 <button type="submit">Créer la tâche</button>
