a6284cde5b63db7df17a93942af3d3478d668f59
David Maggi <musashi.miyamoto81@gmail.com>
2023-12-05 15:38:40 +0100

    AdaptativeLayout - matrix specific case

--- src/lib/AdaptativeLayout.svelte
+++ src/lib/AdaptativeLayout.svelte
@@ -24,14 +24,16 @@
     $header-height: 2.5rem;
     $footer-height: 3rem;
     .alyt-Container {
+        --nonContentHeight: 0; // case without header or footer
         > header, > footer {
             position: fixed;
             z-index: 9999;
         }
         > header { inset: 0 0 auto 0; height: $header-height; background-color: var(--primary); }
         > footer { inset: auto 0 0 0; height: $footer-height; display:flex; justify-content: center; align-items: center; background-color: rgba(216, 27, 96, 0.80); }
-        &.alyt-Container-header { padding-block-start: $header-height; }
-        &.alyt-Container-footer { padding-block-end: $footer-height; }
+        &.alyt-Container-header { padding-block-start: $header-height; --nonContentHeight: #{$header-height}; }
+        &.alyt-Container-footer { padding-block-end: $footer-height; --nonContentHeight: #{$footer-height}; }
+        &.alyt-Container-header.alyt-Container-footer { --nonContentHeight: #{$header-height + $footer-height}; }
 
         width:100%;
         min-height:100vh;
@@ -51,7 +53,14 @@
         }
 
         &.alyt-Content-xLarge {
+            @media (max-width:575px) {
+                height: calc(100vh - var(--nonContentHeight) - 2*(var(--spacing) * 1.25));
+            }
+            @media (min-width: 576px) and (max-width:1199px) {
+                height: calc(100vh - var(--nonContentHeight) - 2*(var(--spacing) * 2));
+            }
             @media (min-width: 1200px) {
+                height: calc(100vh - var(--nonContentHeight) - 2* var(--spacing));
                 max-width: calc(100vw - 3.25 * var(--spacing));
             }
         }
