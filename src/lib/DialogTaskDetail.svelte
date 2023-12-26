0f9a0719a1b98e52532f13053de950bb4f7c15f6
David Maggi <musashi.miyamoto81@gmail.com>
2023-12-26 12:08:32 +0100

    task detail with category

--- src/lib/DialogTaskDetail.svelte
+++ src/lib/DialogTaskDetail.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { taskCategories } from '../constants/task-categories';
     import { appUIState } from '../stores/appUIState';
     import { taskById, type Task } from '../stores/persistentTasks';
     import { Dialog, DialogOverlay, DialogTitle, DialogDescription } from "@rgossiaux/svelte-headlessui";
     import { Eye } from "lucide-svelte";
+    import TaskCategoryIcon from './TaskCategoryIcon.svelte';
     const { clearModal } = appUIState;
     let targetTask:Task|undefined = undefined;
 
@@ -15,13 +17,20 @@
     } else {
         targetTask = undefined;
     }
+    $: currentCategory = taskCategories.find(cat => cat.id === targetTask?.category) ?? null;
 </script>
 
 <Dialog open={ isModalActive } on:close={() => clearModal() }>
     <DialogOverlay class="dlg-Overlay" />
     <article class="dlg-Container">
         <hgroup>
-            <DialogTitle><Eye size={ 32 } /> { targetTask?.label }</DialogTitle>
+            <DialogTitle>
+                {#if currentCategory}
+                    <TaskCategoryIcon name={ currentCategory.icon } stroke-width="1" size="32" color={ currentCategory.color } /> { targetTask?.label }
+                {:else}
+                    <Eye size={ 32 } /> { targetTask?.label }
+                {/if}
+            </DialogTitle>
             <DialogDescription></DialogDescription>
         </hgroup>
         <p>{ targetTask?.description }</p>
