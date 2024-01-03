b4fef7e1adc5599422138fbfc70d41b23eee5588
David Maggi <musashi.miyamoto81@gmail.com>
2024-01-03 22:59:13 +0100

    display duration GUI

---
+++ src/lib/DurationDisplay.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { durationHumanFormater } from "../utils/time-formater";
+
+    export let msDuration:number;
+    export let style:string = '';
+
+    $: timeParts = durationHumanFormater(msDuration);
+</script>
+
+<time class="dd-TimeDisplay" style={ style } datetime="pouet" >
+    {#each timeParts as part}
+        <span class:dd-TimeDisplay_Value={ part.type === 'integer' } class:dd-TimeDisplay_Unit={ part.type === 'literal' }>{ part.value }</span>
+    {/each}
+</time>
+
+<style lang="scss">
+    .dd-TimeDisplay {
+        line-height: 1;
+        .dd-TimeDisplay_Value {
+            font-family: 'Courier New', monospace;
+            font-size: 5vi;
+        }
+
+        .dd-TimeDisplay_Unit { 
+            font-size: 1vi;
+            font-family: var(--font-family);
+        }
+    }
+</style>
--- src/utils/time-formater.ts
+++ src/utils/time-formater.ts
@@ -13,9 +13,10 @@ export const exactDateFormatter:Intl.DateTimeFormat = new Intl.DateTimeFormat('f
     year: "numeric"
 });
 
-export const relativeDateFormatter:Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat('fr-FR', { style: 'long', numeric: 'auto' });
+export const relativeDateFormatter:Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat('fr-FR', { style: 'short', numeric: 'auto' });
+export const durationFormatter:Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat('fr-FR', { style: 'long', numeric: 'always' });
 
-export const relativeHumanFormater = function(fromTimestamp:number, toTimestamp:number):string {
+export const relativeFromToHumanFormater = function(fromTimestamp:number, toTimestamp:number):string {
     const elapsedSecondsSinceCreation:number = (fromTimestamp - toTimestamp) / 1000;
 
     return (Math.abs(elapsedSecondsSinceCreation) > sPerYear)
@@ -27,4 +28,43 @@ export const relativeHumanFormater = function(fromTimestamp:number, toTimestamp:
         : (Math.abs(elapsedSecondsSinceCreation) > sPerHour)
         ? relativeDateFormatter.format(Math.floor(elapsedSecondsSinceCreation / sPerHour), 'hours')
         : relativeDateFormatter.format(Math.floor(elapsedSecondsSinceCreation / sPerMinute), 'minutes')
+}
+
+export const durationHumanFormater = function(msDuration:number):Intl.RelativeTimeFormatPart[] {
+    let secondsDuration:number = (msDuration >= 0) 
+        ? Math.floor(msDuration / 1000)
+        : 0;
+
+    // if 0 leave early
+    if(secondsDuration === 0) return durationFormatter.formatToParts(0, 'second').slice(1);
+
+    let partsResult:Intl.RelativeTimeFormatPart[] = [];
+    switch(true) {
+        case (secondsDuration >= sPerDay):
+            const daysCount:number = Math.floor(secondsDuration / sPerDay);
+            const daysParts = durationFormatter.formatToParts(daysCount, 'day').slice(1); // remove first part 'in', 'ago'
+            partsResult.push(...daysParts);
+            secondsDuration = secondsDuration % sPerDay; // remainder seconds for next case to evaluate
+
+        case (secondsDuration >= sPerHour):
+            const hoursCount:number = Math.floor(secondsDuration / sPerHour);
+            const hoursParts = durationFormatter.formatToParts(hoursCount, 'hour').slice(1);
+            if(0 < hoursCount && hoursCount < 10) hoursParts[0].value = `0${ hoursCount }`; // 2 digit value
+            if(hoursCount > 0) partsResult.push(...hoursParts); // in case there 1 day and 2 seconds (no minutes, no hours)
+            secondsDuration = secondsDuration % sPerHour;
+
+        case (secondsDuration >= sPerMinute):
+            const minutesCount:number = Math.floor(secondsDuration / sPerMinute);
+            const minutesParts = durationFormatter.formatToParts(minutesCount, 'minute').slice(1);
+            if(0 < minutesCount && minutesCount < 10) minutesParts[0].value = `0${ minutesCount }`; // 2 digit value
+            if(minutesCount > 0) partsResult.push(...minutesParts);
+            secondsDuration = secondsDuration % sPerMinute;
+
+        default:
+            const secondsParts = durationFormatter.formatToParts(secondsDuration, 'second').slice(1);
+            if(0 < secondsDuration && secondsDuration < 10) secondsParts[0].value = `0${ secondsDuration }`; // 2 digit value
+            if(secondsDuration > 0) partsResult.push(...secondsParts);
+    }
+
+    return partsResult;
 }
