2f62be8bc8929a8869a7e39e4f6bc430192cf0a7
David Maggi <musashi.miyamoto81@gmail.com>
2024-01-23 22:01:39 +0100

    refactored MaggicChecker Observable

--- src/utils/TaskMaggicCheckerObservable.ts
+++ src/utils/TaskMaggicCheckerObservable.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, combineLatest, distinctUntilKeyChanged, interval, Observable, scan, timeInterval } from "rxjs";
+import { BehaviorSubject, combineLatest, distinctUntilKeyChanged, interval, Observable, scan, timestamp } from "rxjs";
 
 const minDelayThresholdSleepTime:number = 1000; //ms
 const deltaTimeInterval:number = 500; //ms
@@ -8,7 +8,6 @@ type MaggicCheck = {
     sleepState:string // WAKE|SLEEP
     start: number
     end: number
-    clockIteration: number
     sleepDuration: number
     workDuration: number
     pauseDuration: number
@@ -16,37 +15,43 @@ type MaggicCheck = {
 
 export const TaskMaggicCheckerObservable:(bSubject:BehaviorSubject<string>) => Observable<MaggicCheck> = (bSubject:BehaviorSubject<string>) => {
     return combineLatest([bSubject, interval(deltaTimeInterval)]).pipe(
-        timeInterval(),
+        timestamp(),
         scan((acc, curr) => {
-            const [newState, newIteration] = curr.value;
-            const timestamp:number = new Date().getTime();
-            const newStart:number = (acc.start === 0 && newState !== "NOT STARTED") ? timestamp : acc.start; // only once first worked
-            const newEnd:number = timestamp; // update each time
-            const newSleepState:'WAKE'|'SLEEP' = (Math.abs(newEnd - (acc.end + deltaTimeInterval * (newIteration - acc.clockIteration))) > minDelayThresholdSleepTime) ? 'SLEEP' : 'WAKE';
-            let newWorkDuration:number = acc.workDuration;
-            let newPauseDuration:number = acc.pauseDuration;
-            let newSleepDuration:number = acc.sleepDuration;
+            const { value: [state], timestamp } = curr;
+
+            // evaluate all new values
+            const start:number = (acc.start === 0 && state !== "NOT STARTED") ? timestamp : acc.start; // only once first worked
+            const end:number = timestamp; // update each new event
+            const interval:number = timestamp - acc.end;
+
+            // event are every deltaTimeInterval or sooner - if event takes more time this is a sign of sleep
+            const sleepState:'WAKE'|'SLEEP' = (Math.abs(deltaTimeInterval - interval) >= minDelayThresholdSleepTime) ? 'SLEEP' : 'WAKE';
+
+            // accumulate in corresponding bucket
+            let workDuration:number = acc.workDuration;
+            let pauseDuration:number = acc.pauseDuration;
+            let sleepDuration:number = acc.sleepDuration;
             
             switch(true) {
                 // IS SLEEPING
-                case ((acc.sleepState === "WAKE" && newSleepState === "SLEEP") || (acc.sleepState === "SLEEP" && newSleepState === "SLEEP")):
+                case ((acc.sleepState === "WAKE" && sleepState === "SLEEP") || (acc.sleepState === "SLEEP" && sleepState === "SLEEP")):
                     // sleep interval
-                    newSleepDuration += curr.interval;
+                    sleepDuration += interval;
                     break;
 
                 // IS AWAKE
                 case (acc.state === 'WORK'):
                     // was work until now
-                    newWorkDuration += curr.interval;
+                    workDuration += interval;
                     break;
 
                 case (acc.state === 'PAUSE'):
                     // was pause until now
-                    newPauseDuration += curr.interval;
+                    pauseDuration += interval;
             };
 
-            return { state: newState, sleepState: newSleepState, start: newStart, end: newEnd, clockIteration: newIteration, sleepDuration: newSleepDuration, workDuration: newWorkDuration, pauseDuration: newPauseDuration };
-        }, { state: 'NOT STARTED', sleepState: 'WAKE', start:0, end:new Date().getTime(), clockIteration:0, sleepDuration: 0, workDuration: 0, pauseDuration: 0 }),
+            return { state, sleepState, start, end, sleepDuration, workDuration, pauseDuration };
+        }, { state: 'NOT STARTED', sleepState: 'WAKE', start:0, end:0, sleepDuration: 0, workDuration: 0, pauseDuration: 0 }),
         distinctUntilKeyChanged('state', (previousVal, val) => (previousVal === 'NOT STARTED' && val === 'NOT STARTED')), // let pass everything but repeated NOT STARTED
-    )
+    );
 }
