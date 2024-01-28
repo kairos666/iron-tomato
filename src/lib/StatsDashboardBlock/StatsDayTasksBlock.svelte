97bed55bccce3a845dc3d8247b78125caa7d98cb
David Maggi <musashi.miyamoto81@gmail.com>
2024-01-28 22:28:15 +0100

    GUI styles scaffolding

--- src/lib/StatsDashboardBlock/StatsDayTasksBlock.svelte
+++ src/lib/StatsDashboardBlock/StatsDayTasksBlock.svelte
@@ -1,9 +1,134 @@
 <script lang="ts">
-    console.log('day tasks stats block');
+    import { CheckCircle, Star } from "lucide-svelte";
+    import { appUIState } from "../../stores/appUIState";
+    import TaskCategoryIcon from "../TaskCategoryIcon.svelte";
+    import PieChart from "../PieChart.svelte";
+
+    const dayTaskStats = [
+        { label: `Tâche label A`, percent: 43, color: "red" },
+        { label: `Tâche label B`, percent: 21, color: "green" },
+        { label: `Tâche label C`, percent: 19, color: "blue" },
+        { label: `Tâche label D`, percent: 19, color: "aquamarine" }
+    ];
 </script>
 
-<p>day tasks stats</p>
+<article class="sdt-Block">
+    <header>
+        <h2>Activité du jour par tâche</h2>
+    </header>
+    <menu class="sdt-DayTasks" class:sdt-DayTasks-lite={ $appUIState.isMobileViewport }>
+        {#if !$appUIState.isMobileViewport}
+        <div class="sdt-TasksDistrib">
+            <PieChart statistics={ dayTaskStats } baseSize={ 200 } holeSize={ 75 } />
+        </div>
+        {/if}
+        <button class="sdt-TaskBtn">
+            <h3 class="sdt-TaskBtn_Title">Tâche label A</h3>
+            <p class="sdt-TaskBtn_Desc">01h02 travail, 00h14 pause</p>
+            <span class="sdt-TaskBtn_Cat"><TaskCategoryIcon name="hammer" stroke-width="1" size="20" color="var(--h6-color)" /></span>
+            <span class="sdt-TaskBtn_Done"><CheckCircle size="20" color="var(--h6-color)" /></span>
+        </button>
+        <button class="sdt-TaskBtn">
+            <h3 class="sdt-TaskBtn_Title">Tâche label B</h3>
+            <p class="sdt-TaskBtn_Desc">01h02 travail, 00h14 pause</p>
+            <span class="sdt-TaskBtn_Cat"><TaskCategoryIcon name="grape" stroke-width="1" size="20" color="var(--h6-color)" /></span>
+        </button>
+        <button class="sdt-TaskBtn">
+            <h3 class="sdt-TaskBtn_Title">Tâche label C</h3>
+            <p class="sdt-TaskBtn_Desc">01h02 travail, 00h14 pause</p>
+            <span class="sdt-TaskBtn_Cat"><TaskCategoryIcon name="hammer" stroke-width="1" size="20" color="var(--h6-color)" /></span>
+            <span class="sdt-TaskBtn_Done"><CheckCircle size="20" color="var(--h6-color)" /></span>
+            <span class="sdt-TaskBtn_New"><Star size="20" color="var(--h6-color)" /></span>
+        </button>
+        <button class="sdt-TaskBtn">
+            <h3 class="sdt-TaskBtn_Title">Tâche label D</h3>
+            <p class="sdt-TaskBtn_Desc">01h02 travail, 00h14 pause</p>
+            <span class="sdt-TaskBtn_New"><Star size="20" color="var(--h6-color)" /></span>
+        </button>
+    </menu>
+</article>
 
 <style lang="scss">
-    
+    @import "../../styles/page-detail-block";
+
+    .sdt-Block {
+        @include pdb_BlockStyle(h2, false);
+        margin: var(--outer-large-spacing) 0;
+        overflow: hidden;
+    }
+
+    .sdt-DayTasks {
+        padding:0;
+        margin:calc(var(--spacing) * -1);
+
+        @media (max-width:575px) {
+            display: flex;
+            flex-direction: column;
+            gap: 1px;
+        }
+        @media (min-width:576px) {
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-auto-rows: auto;
+        }
+    }
+
+    .sdt-TasksDistrib { 
+        grid-column: 1 / 2; 
+        grid-row: 1 / 666;
+        justify-self: center;
+        align-self: center;
+    }
+
+    .sdt-TaskBtn {
+        grid-column: 2 / 3;
+        margin-block-end: 0;
+        padding: 10px;
+        border-radius: 0;
+        background-color: transparent;
+        border: none;
+        display: grid;
+        gap: 10px;
+        grid-template-columns: 20px 1fr 20px 20px;
+        grid-template-rows: auto auto;
+        grid-template-areas: 
+            "cat title done created"
+            "cat desc desc desc";
+
+        &:hover, &:focus, &:active {
+            background-color: var(--primary-focus);
+        }
+
+        @media (min-width:576px) {
+            border-left:1px solid var(--card-border-color);
+        }
+
+        .sdt-TaskBtn_Title { 
+            grid-area: title;
+            color: var(--h1-color);
+            text-align: left;
+            margin-block-end: 0;
+        }
+        .sdt-TaskBtn_Desc { 
+            grid-area: desc; 
+            color: var(--h6-color);
+            text-align: left;
+            margin-block-end: 0;
+        }
+
+        .sdt-TaskBtn_Cat {
+            grid-area: cat;
+            align-self: center;
+        }
+        .sdt-TaskBtn_Done {
+            grid-area: done;
+        }
+        .sdt-TaskBtn_New {
+            grid-area: created;
+        }
+
+        & + .sdt-TaskBtn {
+            border-top:1px solid var(--card-border-color);
+        }
+    }
 </style>
--- src/lib/StatsDashboardBlock/StatsOverallDayBlock.svelte
+++ src/lib/StatsDashboardBlock/StatsOverallDayBlock.svelte
@@ -1,9 +1,80 @@
 <script lang="ts">
-    console.log('full day stats block');
+    import { CalendarClock, CheckCircle, Coffee, ListTodo, Star } from "lucide-svelte";
+    import { appUIState } from "../../stores/appUIState";
+    import PieChart from "../PieChart.svelte";
+
+    const dayStats = [
+        { label: `Travail`, percent: 63, color: "var(--work-color)" },
+        { label: `Pause`, percent: 37, color: "var(--pause-color)" }
+    ];
 </script>
 
-<p>full day stats</p>
+<section class="sod-Block">
+    {#if $appUIState.isMobileViewport}
+        <span class="sod-Badge sod-Badge-data sod-Badge-mobile">4 <ListTodo size="20" color="var(--badge-color)" /></span>
+        <span class="sod-Badge sod-Badge-data sod-Badge-mobile">3 <Star size="20" color="var(--badge-color)" /></span>
+        <span class="sod-Badge sod-Badge-data sod-Badge-mobile">1 <CheckCircle size="20" color="var(--badge-color)" /></span>
+        <span class="sod-Badge sod-Badge-work sod-Badge-mobile">02h23 <CalendarClock size="20" color="var(--badge-color)" /></span>
+        <span class="sod-Badge sod-Badge-pause sod-Badge-mobile">00h43 <Coffee size="20" color="var(--badge-color)" /></span>
+    {:else}
+    <ul class="sod-Data">
+        <li><span class="sod-Badge sod-Badge-data sod-Badge-desktop">4 <ListTodo size="20" color="var(--badge-color)" /></span> tâches ont avancées</li>
+        <li><span class="sod-Badge sod-Badge-data sod-Badge-desktop">3 <Star size="20" color="var(--badge-color)" /></span> créations de tâches</li>
+        <li><span class="sod-Badge sod-Badge-data sod-Badge-desktop">1 <CheckCircle size="20" color="var(--badge-color)" /></span> tâches terminées</li>
+    </ul>
+    <ul class="sod-StatsLegend">
+        <li><span class="sod-Badge sod-Badge-work sod-Badge-desktop">02h23 <CalendarClock color="var(--badge-color)" /></span></li>
+        <li><span class="sod-Badge sod-Badge-pause sod-Badge-desktop">00h43 <Coffee color="var(--badge-color)" /></span></li>
+    </ul>
+    <PieChart statistics={ dayStats } baseSize={ 200 } holeSize={ 75 } />
+    {/if}
+</section>
 
 <style lang="scss">
-    
+    @import "../../styles/page-detail-badges";
+    .sod-Block {
+        padding: var(--outer-small-spacing);
+        margin-block: var(--outer-large-spacing);
+        display: flex;
+
+        @media (max-width:575px) {
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            gap: var(--inner-small-spacing);
+        }
+        @media (min-width:576px) {
+            justify-content: stretch;
+            align-items: center;
+            gap: var(--outer-large-spacing);
+            > * { flex: 1 1 0; }
+        }
+    }
+
+    .sod-Data {
+        --badge-color: white;
+        --badge-bg-color: goldenrod;
+        padding-inline-start: 0;
+        margin-block-end: 0;
+        li { list-style: none; }
+    }
+    .sod-StatsLegend {
+        padding-inline-start: 0;
+        margin-block-end: 0;
+        li { list-style: none; }
+
+        @media (min-width:576px) {
+            text-align: right;
+        }
+    }
+
+    .sod-Badge {
+        @include pdb_BadgeStyle(var(--badge-color), var(--badge-bg-color));
+        
+        &.sod-Badge-data { --badge-color: white; --badge-bg-color: goldenrod; }
+        &.sod-Badge-work { --badge-color: white; --badge-bg-color: var(--work-color); }
+        &.sod-Badge-pause { --badge-color: white; --badge-bg-color: var(--pause-color); }
+        &.sod-Badge-mobile { padding: 0.25rem 0.75rem; }
+        &.sod-Badge-desktop { padding:5px; }
+    }
 </style>
